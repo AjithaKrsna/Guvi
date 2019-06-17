2b5c9a5873a9b6cceea88ab269b06d6356630544
AjithaKrsna <42969470+AjithaKrsna@users.noreply.github.com>
2019-06-17 17:09:54 +0530

    Create Guvi23MinElement.java

---
+++ Guvi23MinElement.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Largest_Number 
+{
+    public static void main(String[] args) 
+    {
+       int min;
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+         min = arr[0];
+        for(int i = 0; i < n; i++) {
+            if(min > arr[i]){
+                min = arr[i];
+            }
+        }
+        
+        System.out.println(min);
+    }
+}
