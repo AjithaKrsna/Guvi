4c1d8e94233aa52e5bb270f89109598ff302ec98
AjithaKrsna <42969470+AjithaKrsna@users.noreply.github.com>
2019-06-17 10:34:39 +0530

    Create Guvi20first5Mul.java

---
+++ Guvi20first5Mul.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Guvi20first5Mul {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=0;
+        for(int i=0;i<5;i++){
+            m=m+n;
+            System.out.print(m+" ");
+        }
+    }
+}
